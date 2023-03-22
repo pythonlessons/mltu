601f79c8570adeab48c84f4240c41acb3ae0b31a
Rokas <pythonlessons0@gmail.com>
2023-03-22 15:58:04 +0200

    should be tf2onnx instead of self.tf2onnx

--- mltu/tensorflow/callbacks.py
+++ mltu/tensorflow/callbacks.py
@@ -24,7 +24,7 @@ class Model2onnx(Callback):
     def on_train_end(self, logs=None):
         self.model.load_weights(self.saved_model_path)
         self.onnx_model_path = self.saved_model_path.replace(".h5", ".onnx")
-        self.tf2onnx.convert.from_keras(self.model, output_path=self.onnx_model_path)
+        tf2onnx.convert.from_keras(self.model, output_path=self.onnx_model_path)
 
         if self.metadata and isinstance(self.metadata, dict):
             # Load the ONNX model
